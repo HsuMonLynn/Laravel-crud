f2688196e775929ef8c5f1d5f6146e5ca902b91d
HsuMonLynn <scm.hsumonlynn@gmail.com>
2021-12-10 16:52:31 +0630

    fix review

--- app/Http/Requests/PostStoreRequest.php
+++ app/Http/Requests/PostStoreRequest.php
@@ -26,7 +26,7 @@ class PostStoreRequest extends FormRequest
         return [
             'title' => 'required|string|max:255',
             'body' => 'required|string|min:4',
-            'user_id' => 'required|integer'
+            'user_id' => 'required|integer',
         ];
     }
 }
