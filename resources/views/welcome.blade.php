ad7d90a41f7542d6e60b29aa20d7e56ba6a75654
HsuMonLynn <scm.hsumonlynn@gmail.com>
2022-09-27 09:24:41 +0630

    user delete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,12 @@
                                     <h5 class="font-weight-bold">{{ $post->author->name }}</h5>
                                 </div>
                                 <div class="col-md-2">
+                                    <h5 class="font-weight-bold">{{ $post->author->name }}</h5>
+                                </div>
+                                <div class="col-md-2">
+                                    <h5 class="font-weight-bold">{{ $post->author->name }}</h5>
+                                </div>
+                                <div class="col-md-2">
                                     <h5 class="font-weight-bold">Nothing</h5>
                                 </div>
                                 <div class="col-md-2">
